569815d533ef6b96a9d7e481886cae60f71f2cd3
Onur Evren <onur.evren.1999@gmail.com>
2026-02-26 09:15:08 +0300

    fix(test): add placeholder Feature test to prevent PHPUnit directory error

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace ApiLens\Tests\Feature;
+
+use ApiLens\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_true_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
